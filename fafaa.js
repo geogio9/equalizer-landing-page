e60c321768567ba424582471f4c36b3c643ff979
geogio9 <12345678gvianidze@gmail.com>
2022-12-18 21:34:02 +0400

    f

---
+++ fafaa.js
@@ -0,0 +1,5 @@
+// const androidButton=document.querySelector(".android-button");
+// androidButton.addEventListener("mouseout",function(){
+//     androidButton.style.transition="all 0.5s";
+//     console.log("yuju6y")
+// })
